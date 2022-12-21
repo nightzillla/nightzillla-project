7d7eded751a4037ade81739f3daae67e9eac6a2a
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-12-21 12:37:58 -0600

    Update Projects.js

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import Danny from '../../img/Gallery/danny.jpg' 
 
 function Projects() {
   return (
     <div>Projects 
       <div class="page-wrapper">
         <div class="container">
-            <div class="grid grid-cols-4 gap-4">
+          <div class="flex justify-center">
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+              <a href="#!">
+              {/* <NFTcard className="NFTcards"img={naz} name={"naz"} href={"https://github.com/NazarethBriseno"}/> */}
+                <img class={Danny}/>
+                {/* <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/> */}
+              </a>
+              <div class="p-6">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                <p class="text-gray-700 text-base mb-4">
+                  Some quick example text to build on the card title and make up the bulk of the card's
+                  content.
+                </p>
+                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+              </div>
+            </div>
+          </div>
+            <div class="grid grid-flow-row grid-cols-4 gap-4 ">
                 <div class="columns-1 github-finder-card">
                     <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                       <a href="#">
