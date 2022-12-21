31a6cc61bc242d798dd35366a7c6ed2fd8fd02f4
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-12-20 19:25:39 -0600

    updating projects on nightzillla

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -3,11 +3,8 @@ import React from 'react'
 function Projects() {
   return (
     <div>Projects 
-    <div>
-    {/* <iframe src='https://my.spline.design/colorbox3copy-60e7ddf7671ece5affb63092b740fdc6/' frameborder='0' width='300' height='300'></iframe> */}
-    </div>
-    <a href= "https://github-finder-firebase.web.app/"> Github Finder</a>
-    <a href="https://bros-movie.firebaseapp.com/">Bro Movie</a>
+      <a href= "https://github-finder-firebase.web.app/"> Github Finder</a>
+      <a href="https://bros-movie.firebaseapp.com/">Bro Movie</a>
     </div>
   )
 }
