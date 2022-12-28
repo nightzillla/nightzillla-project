3e12083dc115968827029747fca4c10be89f2a90
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-12-28 14:24:53 -0600

    updating img in profile

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -15,14 +15,14 @@ function Projects() {
             {/* <div class= "flex justify-center grid grid-cols-6 xs:grid-cols-6 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4"> */}
             {/* <div class= "grid grid-cols-2 grid-flow-col gap-4"></div> */}
           </div> {/* END OF CONTAINER */}
-          <div class= "grid sm:grid-cols-3 md:grid-cols-3 py-5">
+          <div class= "flex justify-center grid sm:grid-cols-3 md:grid-cols-3 py-5">
             <div class="flex justify-center py-5 pr-4">
               <GithhubFinder/>
             </div> {/* END OF GithubFinder */}
-            <div class="flex justify-center py-5 pr-2">
+            <div class="flex justify-center py-5">
               <BrosMovies/>
             </div>
-            <div class="flex justify-center py-5 pr-2">
+            <div class="flex justify-center py-5">
               <BrosMovies/>
             </div>
           </div>{/* END OF GRID */}
