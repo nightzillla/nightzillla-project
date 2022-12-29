f39abc1b43d4308f16121d1acc5ae73e274fc889
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-12-29 13:04:24 -0600

    Update ProfileCard.js

--- src/pages/Projects/ProfileCards/ProfileCard.js
+++ src/pages/Projects/ProfileCards/ProfileCard.js
@@ -12,8 +12,13 @@ function ProfileCard() {
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 I am a dedicated Software Developer and Army Veteran with a love for coding, teamwork, and creative thinking. 
                 </p>
+                <div>
                   <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   <a href="https://www.linkedin.com/in/danny-nguyen-nightzillla/">Linkedin</a>
+                  </a>
+                </div>
+                <a href="#" class="inline-flex items-center pl-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  <a href="https://www.linkedin.com/in/danny-nguyen-nightzillla/">Github</a>
                 </a>
                 </div>
             </div>
