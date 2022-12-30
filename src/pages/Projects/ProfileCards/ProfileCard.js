6587b75ddac7704ddfa6f1ba1b348b8f26f973a4
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-12-30 16:41:33 -0600

    adding logo

--- src/pages/Projects/ProfileCards/ProfileCard.js
+++ src/pages/Projects/ProfileCards/ProfileCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Danny from '../../../img/Gallery/danny.jpg' 
+import Danny from '../../../img/Gallery/danny.jpg'
+import {FaLinkedin} from "react-icons/fa"
 
 function ProfileCard() {
   return (
@@ -13,9 +14,10 @@ function ProfileCard() {
                 I am a dedicated Software Developer and Army Veteran with a love for coding, teamwork, and creative thinking. 
                 </p>
                 <div>
-                  <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  <a href="https://www.linkedin.com/in/danny-nguyen-nightzillla/">Linkedin</a>
-                  </a>
+                  {/* <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> */}
+                  <FaLinkedin style="white"/>
+                  <a href="https://www.linkedin.com/in/danny-nguyen-nightzillla/">{FaLinkedin}</a>
+                  {/* </a> */}
                 </div>
                   <a href="#" class="inline-flex items-center pl-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     <a href="https://www.linkedin.com/in/danny-nguyen-nightzillla/">Github</a>
