27e1a165d925b4fb67b2a3dcf4c810461b09b369
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-12-31 00:27:43 -0600

    updating profile links

--- src/pages/Projects/ProfileCards/ProfileCard.js
+++ src/pages/Projects/ProfileCards/ProfileCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Danny from '../../../img/Gallery/danny.jpg'
 import {FaLinkedin} from "react-icons/fa"
+import {FaGithubSquare} from "react-icons/fa"
 
 function ProfileCard() {
   return (
@@ -9,21 +10,18 @@ function ProfileCard() {
             <div class="rounded-lg shadow-lg bg-white max-w-sm shadow-md dark:bg-gray-800 dark:border-gray-700">
                 <img class={"Danny rounded-lg"} src={Danny}/>
                 <div class="p-6">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Danny Nguyen</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                I am a dedicated Software Developer and Army Veteran with a love for coding, teamwork, and creative thinking. 
-                </p>
-                <div>
-                  {/* <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> */}
-                  <a href="https://www.linkedin.com/in/danny-nguyen-nightzillla/">
-                  <FaLinkedin className="text-3xl text-white"/>
-
-                  </a>
-                  {/* </a> */}
-                </div>
-                  <a href="#" class="inline-flex items-center pl-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <a href="https://www.linkedin.com/in/danny-nguyen-nightzillla/">Github</a>
-                  </a>
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Danny Nguyen</h5>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  I am a dedicated Software Developer and Army Veteran with a love for coding, teamwork, and creative thinking. 
+                  </p>
+                  <div className="flex px-5">
+                    <a href="https://www.linkedin.com/in/danny-nguyen-nightzillla/">
+                      <FaLinkedin className="text-3xl text-white"/>
+                    </a>
+                    <a href="https://github.com/nightzillla">
+                      <FaGithubSquare className="text-3xl text-white"/>
+                    </a>
+                  </div>
                 </div>
             </div>
         </div> {/* END OF DANNY PROFILE CARD */}
