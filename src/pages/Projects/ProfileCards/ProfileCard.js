181b9a9513be5133d2742785eb8bb463eb8e17dc
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-12-31 00:21:18 -0600

    Updating profile linkedin

--- src/pages/Projects/ProfileCards/ProfileCard.js
+++ src/pages/Projects/ProfileCards/ProfileCard.js
@@ -15,8 +15,10 @@ function ProfileCard() {
                 </p>
                 <div>
                   {/* <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> */}
-                  <FaLinkedin/>
-                  <a href="https://www.linkedin.com/in/danny-nguyen-nightzillla/">{FaLinkedin}</a>
+                  <a href="https://www.linkedin.com/in/danny-nguyen-nightzillla/">
+                  <FaLinkedin className="text-3xl text-white"/>
+
+                  </a>
                   {/* </a> */}
                 </div>
                   <a href="#" class="inline-flex items-center pl-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
