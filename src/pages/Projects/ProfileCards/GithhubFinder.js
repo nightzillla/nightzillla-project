94a78b6d2766338009162e3a46efc13b45db2761
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-04 13:29:03 -0600

    updating profile cards

--- src/pages/Projects/ProfileCards/GithhubFinder.js
+++ src/pages/Projects/ProfileCards/GithhubFinder.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../Project.css'
-import {FaPlay} from 'react-icons/fa'
 
 function GithhubFinder() {
 
@@ -13,7 +12,15 @@ function GithhubFinder() {
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
             Github Finder is a solo project that was developed in React, that uses the github API to find users github profiles and repositories.
             </p>
-              <a target="_blank" href= "https://github-finder-firebase.web.app/"><FaPlay className="text-3xl text-gray-400 hover:bg-white mr-[.5rem]"/></a>
+            <div className="flex">
+            <a className="mr-[.5rem]"target="_blank" href= "https://github-finder-firebase.web.app/">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play-btn-fill" viewBox="0 0 16 16"><path d="M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6.79-6.907A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
+              </svg>
+              </a>
+              <a target="_blank" href="https://github.com/nightzillla/github-finder-firebase">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-code-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z"/></svg>
+              </a>
+            </div>
           </div>
         </div>{/* END OF PROFILE CARD*/}
       </div> {/* END OF GITHUB FINDER CARD */}
