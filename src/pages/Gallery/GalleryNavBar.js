cec32a57db83a2289040af4def35153cf1a206f9
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-06 21:46:11 -0600

    updating profile cards and css

--- src/pages/Gallery/GalleryNavBar.js
+++ src/pages/Gallery/GalleryNavBar.js
@@ -6,23 +6,17 @@ import {Link} from 'react-router-dom'
 function GalleryNavBar() {
   return (
     <>
-        <ul className="flex justify-evenly justify-center galleryNavBar ">
+        <ul className="flex justify-evenly galleryNavBar ">
             <h1 className="galleryNightzillaFont pr-12">nightzillla</h1>
-            <li className="homeGalleryNavBar dark:text-gray-400">
+            <li className="galleryHomeGalleryNavBar dark:text-gray-400">
             <Link to="/"><h4>Home</h4></Link>
             </li>
-            <li className='aboutMeNavBar dark:text-gray-400'>
+            <li className='galleryProfileNavBar dark:text-gray-400'>
             <Link to="/aboutme"><h4>Profile</h4></Link>
             </li>
-            {/* <li className='galleryNavBars dark:text-gray-400'>
-            <Link to="/gallery"><h4>Gallery</h4></Link>
-            </li> */}
-            <li className='cohortNavBar dark:text-gray-400'>
+            <li className='galleryCohortNavBar dark:text-gray-400'>
             <Link to="/cohort"><h4>Cohort</h4></Link>
             </li>
-            {/* <li className="projectNavBar dark:text-gray-400">
-            <Link to="/projects"><h4>Projects</h4></Link>
-            </li> */}
         </ul>
     </>
   )
--- src/pages/Projects/ProjectNavBar.jsx
+++ src/pages/Projects/ProjectNavBar.jsx
@@ -11,7 +11,7 @@ function ProjectNavBar() {
             <li className="homeProjectNavBar dark:text-gray-400">
             <Link to="/"><h4>Home</h4></Link>
             </li>
-            <li className='galleryNavBars dark:text-gray-400'>
+            <li className='galleryNavBar dark:text-gray-400'>
             <Link to="/gallery"><h4>Gallery</h4></Link>
             </li>
             <li className='cohortNavBar dark:text-gray-400'>
